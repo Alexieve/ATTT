66d1a42c2270fce28d1b7a70d704e5c56bc9cd6a
tvphuc21 <100831107+tvphuc21@users.noreply.github.com>
2024-04-19 01:29:02 +0700

    Add files via upload

---
+++ Sources/ATTT/PH2/BLL/XemPhanCongBLL.cs
@@ -0,0 +1,26 @@
+﻿using PH2.DAL;
+using PH2.DTO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PH2.BLL
+{
+    internal class XemPhanCongBLL
+    {
+        private XemPhanCongDAL XemPhanCongDAL;
+        public XemPhanCongBLL()
+        {
+            XemPhanCongDAL = new XemPhanCongDAL();
+        }
+        public List<XemPhanCongDTO> getPhanCong()
+        {
+            DataTable dt = XemPhanCongDAL.GetPhanCong();
+            List<XemPhanCongDTO> danhSach = Utility.ToList<XemPhanCongDTO>(dt);
+            return danhSach;
+        }
+    }
+}
