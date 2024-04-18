fe6c29a526edbcec3baa2a7bba80f26fce9cf6f4
tvphuc21 <100831107+tvphuc21@users.noreply.github.com>
2024-04-19 01:30:12 +0700

    Add files via upload

---
+++ Sources/ATTT/PH2/GUI/GIANGVIEN/GV_XemDangKy.cs
@@ -0,0 +1,94 @@
+﻿using PH2.BLL;
+using PH2.DTO;
+using System.Data;
+using System.Windows.Forms;
+
+namespace PH2.GUI.GIANGVIEN
+{
+    public partial class GV_XemDangKy : Form
+    {
+        string prevValue;
+        private GV_XemDangKyBLL XemDangKyBLL;
+        public GV_XemDangKy()
+        {
+            InitializeComponent();
+            XemDangKyBLL = new GV_XemDangKyBLL();
+            Load();
+        }
+        private void Load()
+        {
+            dataGridView1.Columns.Clear();
+            List<GV_XemDangKyDTO> danhSachDK = XemDangKyBLL.GetDangKyGV();
+            //foreach (var array in danhSachDK)
+            //{
+            //    dataGridView1.Rows.Add(array.MASV, array.MAGV, array.HOTEN, array.MAHP, array.TENHP, array.HK, array.NAM, array.MACT, array.DIEMTH, array.DIEMQT, array.DIEMCK, array.DIEMTK);
+            //}
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = danhSachDK;
+            setColumnName();
+        }
+        private void setColumnName()
+        {
+            dataGridView1.Columns[0].HeaderText = "Mã Sinh Viên";
+            dataGridView1.Columns[1].HeaderText = "Mã Giáo Viên";
+            dataGridView1.Columns[0].Visible = false;
+            dataGridView1.Columns[1].Visible = false;
+            dataGridView1.Columns[2].HeaderText = "Tên Sinh Viên";
+            dataGridView1.Columns[3].HeaderText = "Mã Học Phần";
+            dataGridView1.Columns[3].Visible = false;
+            dataGridView1.Columns[4].HeaderText = "Tên Học Phần";
+            dataGridView1.Columns[5].HeaderText = "Học Kì";
+            dataGridView1.Columns[6].HeaderText = "Năm";
+            dataGridView1.Columns[7].HeaderText = "Mã Chương Trình";
+            dataGridView1.Columns[8].HeaderText = "Điểm Thực Hành";
+            dataGridView1.Columns[9].HeaderText = "Điểm Quá Trình";
+            dataGridView1.Columns[10].HeaderText = "Điểm Cuối Kì";
+            dataGridView1.Columns[11].HeaderText = "Điểm Tổng Kết";
+            for (int i = 0; i < 8; i++)
+            {
+                dataGridView1.Columns[i].ReadOnly = true;
+            }
+        }
+
+        private void dataGridView1_DataError(object sender, DataGridViewDataErrorEventArgs e)
+        {
+            MessageBox.Show("Điểm không hợp lệ!", "Thông báo");
+            dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value = prevValue;
+        }
+
+        private void dataGridView1_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            string DIEM = dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value.ToString();
+
+            if (!XemDangKyBLL.CheckConvert(DIEM))
+            {
+                MessageBox.Show("Điểm không hợp lệ!", "Thông báo");
+                dataGridView1.Rows[e.RowIndex].Cells[e.ColumnIndex].Value = prevValue;
+                return;
+            }
+            string MASV = dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString();
+            string MAGV = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+            string HOTEN = dataGridView1.Rows[e.RowIndex].Cells[2].Value.ToString();
+            string MAHP = dataGridView1.Rows[e.RowIndex].Cells[3].Value.ToString();
+            string TENHP = dataGridView1.Rows[e.RowIndex].Cells[4].Value.ToString();
+            string tmp = dataGridView1.Rows[e.RowIndex].Cells[5].Value.ToString();
+            string DIEMTH = dataGridView1.Rows[e.RowIndex].Cells[8].Value.ToString();
+            string DIEMQT = dataGridView1.Rows[e.RowIndex].Cells[9].Value.ToString();
+            string DIEMCK = dataGridView1.Rows[e.RowIndex].Cells[10].Value.ToString();
+            string DIEMTK = dataGridView1.Rows[e.RowIndex].Cells[11].Value.ToString();
+            int HK = Int32.Parse(tmp);
+            tmp = dataGridView1.Rows[e.RowIndex].Cells[6].Value.ToString();
+            int NAM = Int32.Parse(tmp);
+            string MACT = dataGridView1.Rows[e.RowIndex].Cells[7].Value.ToString();
+            bool temp = float.TryParse(DIEMTH, out float d);
+            temp = float.TryParse(DIEMQT, out float f);
+            temp = float.TryParse(DIEMCK, out float g);
+            temp = float.TryParse(DIEMTK, out float h);
+            XemDangKyBLL.UpdateDiem(MASV, MAGV, MAHP, HK, NAM, MACT, d, f, g, h);
+        }
+        private void dataGridView1_CellBeginEdit(object sender, DataGridViewCellCancelEventArgs e)
+        {
+            prevValue = dataGridView1[e.ColumnIndex, e.RowIndex].Value.ToString();
+        }
+    }
+}
---
+++ Sources/ATTT/PH2/GUI/GIANGVIEN/XemPhanCong.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace PH2.GUI.GIANGVIEN
+{
+    partial class XemPhanCong
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            DanhSachPC = new DataGridView();
+            TenHP = new DataGridViewTextBoxColumn();
+            Nam = new DataGridViewTextBoxColumn();
+            MaCT = new DataGridViewTextBoxColumn();
+            HK = new DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)DanhSachPC).BeginInit();
+            SuspendLayout();
+            // 
+            // DanhSachPC
+            // 
+            DanhSachPC.AllowUserToAddRows = false;
+            DanhSachPC.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = SystemColors.Control;
+            dataGridViewCellStyle1.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 163);
+            dataGridViewCellStyle1.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.True;
+            DanhSachPC.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            DanhSachPC.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            DanhSachPC.Columns.AddRange(new DataGridViewColumn[] { TenHP, Nam, MaCT, HK });
+            DanhSachPC.Location = new Point(0, 0);
+            DanhSachPC.Name = "DanhSachPC";
+            DanhSachPC.ReadOnly = true;
+            DanhSachPC.RowHeadersWidth = 51;
+            DanhSachPC.Size = new Size(1316, 653);
+            DanhSachPC.TabIndex = 0;
+            // 
+            // TenHP
+            // 
+            TenHP.HeaderText = "Tên học phần";
+            TenHP.MinimumWidth = 6;
+            TenHP.Name = "TenHP";
+            TenHP.ReadOnly = true;
+            // 
+            // Nam
+            // 
+            Nam.HeaderText = "Năm";
+            Nam.MinimumWidth = 6;
+            Nam.Name = "Nam";
+            Nam.ReadOnly = true;
+            // 
+            // MaCT
+            // 
+            MaCT.HeaderText = "Chương trình";
+            MaCT.MinimumWidth = 6;
+            MaCT.Name = "MaCT";
+            MaCT.ReadOnly = true;
+            // 
+            // HK
+            // 
+            HK.HeaderText = "Học kì";
+            HK.MinimumWidth = 6;
+            HK.Name = "HK";
+            HK.ReadOnly = true;
+            // 
+            // XemPhanCong
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1315, 652);
+            Controls.Add(DanhSachPC);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "XemPhanCong";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)DanhSachPC).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView DanhSachPC;
+        private DataGridViewTextBoxColumn TenHP;
+        private DataGridViewTextBoxColumn Nam;
+        private DataGridViewTextBoxColumn MaCT;
+        private DataGridViewTextBoxColumn HK;
+    }
+}
