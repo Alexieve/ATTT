c236db4307a4ab739da418a726c6cb723a862e47
tvphuc21 <100831107+tvphuc21@users.noreply.github.com>
2024-04-19 01:28:37 +0700

    Add files via upload

---
+++ Sources/ATTT/PH2/DAL/GV_XemDangKyDAL.cs
@@ -0,0 +1,46 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PH2.DAL
+{
+    internal class GV_XemDangKyDAL
+    {
+        private DbConnection conn;
+        public GV_XemDangKyDAL()
+        {
+            conn = new DbConnection();
+        }
+        public DataTable GetDangKyGV()
+        {
+            string procName = "USP_GET_DANG_KY_GV";
+            OracleParameter[] Parameters =
+            {
+                new("P_RES", OracleDbType.RefCursor, ParameterDirection.Output)
+            };
+            return conn.ExecuteQuery(procName, CommandType.StoredProcedure, Parameters);
+        }
+        public void UpdateDiem(string MASV, string MAGV, string MAHP, int HK, int NAM, string MACT, float DIEMTH, float DIEMQT, float DIEMCK, float DIEMTK)
+        {
+            string procName = "USP_GV_UPDATE_DIEM";
+            OracleParameter[] Parameters =
+            {
+                new("P_MASV", OracleDbType.Varchar2, MASV, ParameterDirection.Input),
+                new("P_MAGV", OracleDbType.Varchar2, MAGV, ParameterDirection.Input),
+                new("P_MAHP", OracleDbType.Varchar2, MAHP, ParameterDirection.Input),
+                new("P_HK", OracleDbType.Int32, HK, ParameterDirection.Input),
+                new("P_NAM", OracleDbType.Int32, NAM, ParameterDirection.Input),
+                new("P_MACT", OracleDbType.Varchar2, MACT, ParameterDirection.Input),
+                new("P_DIEMTH", OracleDbType.Single, DIEMTH, ParameterDirection.Input),
+                new("P_DIEMQT", OracleDbType.Single, DIEMQT, ParameterDirection.Input),
+                new("P_DIEMCK", OracleDbType.Single, DIEMCK, ParameterDirection.Input),
+                new("P_DIEMTK", OracleDbType.Single, DIEMTK, ParameterDirection.Input)
+            };
+            conn.ExecuteNonQuery(procName, CommandType.StoredProcedure, Parameters);
+        }
+    }
+}
