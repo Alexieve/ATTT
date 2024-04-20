d752c02eea12c2bf1d6d3f03e371d400306d9912
Thinh Vuong <94957063+thinhcu67si@users.noreply.github.com>
2024-04-20 17:16:10 +0700

    Update DangKyHPDAL.cs

--- Sources/ATTT/PH2/DAL/DangKyHPDAL.cs
+++ Sources/ATTT/PH2/DAL/DangKyHPDAL.cs
@@ -57,5 +57,52 @@ namespace PH2.DAL
             };
             conn.ExecuteNonQuery(procName, CommandType.StoredProcedure, Parameters);
         }
+                
+        public DataTable getChuaDK(string a)
+        {
+            string procName = "USP_DANGKYHP_GET_CHUADK_GVU";
+            OracleParameter[] Parameters =
+            {
+                new OracleParameter("P_MASV", OracleDbType.Varchar2, a, ParameterDirection.Input ),
+                new("P_RES", OracleDbType.RefCursor, ParameterDirection.Output)
+            };
+            return conn.ExecuteQuery(procName, CommandType.StoredProcedure, Parameters);
+        }
+                
+        public DataTable getDaDK2(string a)
+        {
+            string procName = "USP_DANGKYHP_GET_DADK_GVU";
+            //string tmp = "SV00000102";
+            OracleParameter[] Parameters =
+            {
+                new("P_MASV", OracleDbType.Varchar2, a, ParameterDirection.Input ),
+                new("P_RES", OracleDbType.RefCursor, ParameterDirection.Output)
+            };
+            return conn.ExecuteQuery(procName, CommandType.StoredProcedure, Parameters);
+        }
+                
+        public void HuyHP(string MAGV, string MAHP, string a)
+        {
+            string procName = "USP_DANGKYHP_HUY_SV_GVU";
+            OracleParameter[] Parameters =
+            {
+                new OracleParameter("P_MASV", OracleDbType.Varchar2, a, ParameterDirection.Input ),
+                new("P_MAGV", OracleDbType.Varchar2, MAGV, ParameterDirection.Input),
+                new("P_MAHP", OracleDbType.Varchar2, MAHP, ParameterDirection.Input)
+            };
+            conn.ExecuteNonQuery(procName, CommandType.StoredProcedure, Parameters);
+        }
+                
+        public void DangKyHP(string MAGV, string MAHP, string a)
+        {
+            string procName = "USP_DANGKYHP_DANGKY_SV_GVU";
+            OracleParameter[] Parameters =
+            {
+                new OracleParameter("P_MASV", OracleDbType.Varchar2, a, ParameterDirection.Input ),
+                new("P_MAGV", OracleDbType.Varchar2, MAGV, ParameterDirection.Input),
+                new("P_MAHP", OracleDbType.Varchar2, MAHP, ParameterDirection.Input)
+            };
+            conn.ExecuteNonQuery(procName, CommandType.StoredProcedure, Parameters);
+        }
     }
 }
